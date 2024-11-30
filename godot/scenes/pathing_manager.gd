5652289d361385dc42ad264dd5c1fefb8cf0f3cd
justfordoener <46346088+justfordoener@users.noreply.github.com>
2024-11-30 19:36:08 +0100

    pathing update mit bockbar

--- godot/scenes/pathing_manager.gd
+++ godot/scenes/pathing_manager.gd
@@ -3,7 +3,8 @@ extends Node
 @onready var grid_map : GridMap = $"../GridMap"
 
 var MOVEMENT_SPEED := 1
-@onready var NPC_COUNT := 1
+var NPC_COUNT := 1
+var BOCK_COST := -10
 var movement_speed_0 = MOVEMENT_SPEED
 var movement_speed_1 = MOVEMENT_SPEED
 var movement_speed = [NPC_COUNT]
@@ -15,11 +16,12 @@ var is_moving = [NPC_COUNT]
 var path_follow = [NPC_COUNT]
 @onready var raycast_0 : RayCast3D = $"../Path3D/PathFollow3D/MeshInstance3D/StaticBody3D/RayCast3D"
 @onready var raycast_1 : RayCast3D #= $"../Path3D/PathFollow3D/MeshInstance3D/StaticBody3D/RayCast3D"
+var raycast = [NPC_COUNT]
+@onready var npc_0 : Node3D = $"../Path3D/PathFollow3D/MeshInstance3D/StaticBody3D/Npc"
 var anim_0_walk : AnimationPlayer
 var anim_0_idle : AnimationPlayer
 var anim_0_shovel : AnimationPlayer
-var raycast = [NPC_COUNT]
-@onready var npc_0 : Node3D = $"../Path3D/PathFollow3D/MeshInstance3D/StaticBody3D/Npc"
+var bock_bar = [NPC_COUNT]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -36,7 +38,8 @@ func _ready() -> void:
 	anim_0_idle = npc_0.get_child(0).get_child(1)
 	anim_0_walk = npc_0.get_child(0).get_child(2)
 	anim_0_shovel = npc_0.get_child(0).get_child(3)
-	
+	bock_bar[0] = npc_0.get_child(1)
+	bock_bar[0].value = 100	
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -44,27 +47,63 @@ func _process(delta: float) -> void:
 	# move npc #0
 	_move_npc(delta, 0) #is_moving_0, path_follow_0, movement_speed_0)
 	_check_collision(0) #raycast_0, is_moving_0)
+	_update_bock_bar(delta, 0)
 	
 	# move npc #1
 	#_move_npc(delta, is_moving_1 ,path_follow_1, movement_speed_1)
 	#_check_collision(raycast_1, is_moving_1)
 	
 
-
 func _check_collision(num: int) -> void:
 	if raycast[num].is_colliding() and is_moving[num]:
 			is_moving[num] = false
 			var col = raycast[num].get_collider()
 			if col:
-				print("collided with: " + col.name)
+				movement_speed[num] = 0
 				await get_tree().create_timer(2.0).timeout
 				col.get_owner().queue_free()
+				movement_speed[num] = 0
 			is_moving[num] = true
+			movement_speed[num] = MOVEMENT_SPEED
 			
 
-func _move_npc(delta: float, num: int) -> void:
-	if is_moving[num]:
-		anim_0_walk.play("Walkcycle")
-		path_follow[num].progress += delta * movement_speed[num]
+func _update_bock_bar(delta: float, num: int) -> void:
+	if bock_bar[num].value == 0:
+		# walk home
+		if anim_0_shovel.is_playing():
+			anim_0_shovel.stop()
+		if !anim_0_walk.is_playing():
+			anim_0_walk.play("Walkcycle")
+		movement_speed[num] = -1.5 * MOVEMENT_SPEED
+		if (path_follow[num].progress == 0.0) and is_moving[num]:
+			is_moving[num] = false
+			#regenerate bock
+			await get_tree().create_timer(5.0).timeout
+			path_follow[num].progress = 0.1
+			bock_bar[num].value = 100
+			is_moving[num] = true	
+			movement_speed[num] = MOVEMENT_SPEED
 	else:
-		anim_0_shovel.play("Shovel")
+		bock_bar[num].value += delta * BOCK_COST
+		
+func _move_npc(delta: float, num: int) -> void:
+	# TODO make anim an array for each NPC
+	path_follow[num].progress += delta * movement_speed[num]
+	if (path_follow[num].progress == 0):
+		if is_moving[num]:
+			if !anim_0_walk.is_playing():
+				anim_0_walk.play("Walkcycle")
+		else: 
+			if anim_0_walk.is_playing():
+				anim_0_walk.stop()
+			if !anim_0_idle.is_playing():
+				anim_0_idle.play("Idle")
+	else:
+		if is_moving[num]:
+			if !anim_0_walk.is_playing():
+				anim_0_walk.play("Walkcycle")
+		else:
+			if anim_0_walk.is_playing():
+				anim_0_walk.stop()
+			if !anim_0_shovel.is_playing():
+				anim_0_shovel.play("Shovel")
